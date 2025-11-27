94935999ca34533c8636a6bf966bff0e2a13c2b3
eustaquio <eustaquio.galano@gmail.com>
2025-11-27 22:26:17 +0800

    refactor: add component for input and label

--- src/components/Main/Section.jsx
+++ src/components/Main/Section.jsx
@@ -1,17 +1,32 @@
+function InputAndLabel({
+  htmlFor,
+  text,
+  type = "text",
+  name,
+  id,
+  placeholder = "",
+}) {
+  return (
+    <div>
+      <label htmlFor={htmlFor}>{text}</label>
+      <input type={type} name={name} id={id} placeholder={placeholder} />
+    </div>
+  );
+}
+
 function Section() {
   return (
     <section>
       <h1>Login</h1>
-      <form action="">
-        <div>
-          <label htmlFor="id-number">ID Number:</label>
-          <input type="text" name="id-number" id="id-number" />
-        </div>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" name="name" id="name" />
-        </div>
-        <div>
+      <form action="" id="login-form">
+        <InputAndLabel htmlFor="name" text="Name: " name="name" id="name" />
+        <InputAndLabel
+          htmlFor="id-number"
+          text="ID Number: "
+          name="id-number"
+          id="id-number"
+        />
+        <div id="login-register-container">
           <button>Login</button>
           <p>or</p>
           <button>Register</button>
