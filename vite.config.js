2f0966c2b98d739e8430b3825d6961c3858fd3d2
eustaquio <eustaquio.galano@gmail.com>
2025-11-30 15:54:39 +0800

    chore: update repo name in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/eustaquiogalano/calendar-vmc", // replace with repo name
+  base: "/calendar-vmc/", // replace with repo name
 });
