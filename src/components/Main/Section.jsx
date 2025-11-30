8fb9512acaf3e6542bd5ee27b82e1328aa002a34
eustaquio <eustaquio.galano@gmail.com>
2025-11-30 15:41:03 +0800

    feat: add register section

--- src/components/Main/Section.jsx
+++ src/components/Main/Section.jsx
@@ -33,6 +33,35 @@ function Section() {
         </div>
       </form>
     </section>
+        <section id="register-section">
+          <h2>Register</h2>
+          <form action="" id="register-form">
+            <InputAndLabel htmlFor="name" text="Name: " name="name" id="name" />
+            <InputAndLabel
+              htmlFor="id-number"
+              text="ID Number: "
+              name="id-number"
+              id="id-number"
+            />
+            <InputAndLabel
+              htmlFor="email"
+              text="Email: "
+              name="email"
+              id="email"
+            />
+            <InputAndLabel
+              htmlFor="last-year-attended"
+              text="Last Year Attended: "
+              name="last-year-attended"
+              id="last-year-attended"
+            />
+            <div id="register-button-container">
+              <button>Register</button>
+            </div>
+          </form>
+        </section>
+      </div>
+    </div>
   );
 }
 
