8cb3ba9d7ffea99d7885026b032c8e1eaddb96fc
eustaquio <eustaquio.galano@gmail.com>
2025-12-02 13:25:20 +0800

    feat: import and use StudentUI component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
+import { StudentInterface } from "../../StudentUI/StudentUI";
 import "./Main.css";
 import { LoginRegisterForm } from "./Section";
 
 function PageMain() {
   return (
     <main>
-      <LoginRegisterForm />
+      {/* <LoginRegisterForm /> */}
+      <StudentInterface />
     </main>
   );
 }
