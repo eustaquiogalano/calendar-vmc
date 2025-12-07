70f95d0a294f81555292749fa0ddcfdf424fc330
eustaquio <eustaquio.galano@gmail.com>
2025-12-07 10:58:53 +0800

    style: update component name and use css module
    - updated class names for css module

---
+++ src/components/PageHeader/PageHeader.jsx
@@ -0,0 +1,13 @@
+import vmcIcon from "../../assets/vmc-icon.png";
+import style from "./PageHeader.module.css";
+
+function PageHeader() {
+  return (
+    <header className={style["page-header"]}>
+      <img className={style["page-header__img"]} src={vmcIcon} alt="" />
+      <h1 className={style["page-header__h1"]}>Calendar</h1>
+    </header>
+  );
+}
+
+export { PageHeader };
