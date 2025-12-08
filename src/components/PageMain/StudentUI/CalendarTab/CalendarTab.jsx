bc13fa161111d1219d12ad168988c9a6193330c3
eustaquio <eustaquio.galano@gmail.com>
2025-12-08 13:04:23 +0800

    feat: add CalendarTab component

---
+++ src/components/PageMain/StudentUI/CalendarTab/CalendarTab.jsx
@@ -0,0 +1,33 @@
+import style from "./CalendarTab.module.css";
+import HourCard from "./Calender/HourCard/HourCard";
+import Calendar from "./Calender/Calendar";
+
+function CalendarTab() {
+  return (
+    <>
+      <section className={style["calendar-tab__section"]}>
+        <h2>Calendar</h2>
+        <div
+          className={`${style["calendar-tab__calendar"]} ${style["calendar-tab__calendar--shadow"]}`}
+        >
+          <Calendar />
+        </div>
+      </section>
+
+      <section className={style["calendar-tab__section"]}>
+        <h2>Events</h2>
+        <div
+          className={`${style["calendar-tab__events"]} ${style["calendar-tab__events--shadow"]}`}
+        >
+          <HourCard
+            time="8:00 AM"
+            eventList={["Seminar sa CR", "Pizza Party"]}
+          />
+          <HourCard time="10:00 AM" eventList={["Oath Taking", "Disco Time"]} />
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default CalendarTab;
