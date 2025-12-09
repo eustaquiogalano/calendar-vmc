6ca998d755fc433c6d226456e615b4748867d414
eustaquio <eustaquio.galano@gmail.com>
2025-12-09 23:55:19 +0800

    feat: add student state and handleLogin function

--- src/components/PageMain/PageMain.jsx
+++ src/components/PageMain/PageMain.jsx
@@ -1,8 +1,15 @@
 import { StudentUI } from "./StudentUI/StudentUI";
 import style from "./PageMain.module.css";
 import Login from "./Login/Login";
+import { useState } from "react";
 
 function PageMain() {
+  const [student, setStudent] = useState(undefined);
+
+  function handleLogin(user) {
+    setStudent(user);
+  }
+
   return (
     <main
       className={`${style["page-main"]} ${style["page-main--hide-scroll"]}`}
