ed791f92947fdc5888abcf3b5281850ed2b4bd84
eustaquio <eustaquio.galano@gmail.com>
2025-12-17 20:35:56 +0800

    refactor: replace implementation for UI demo

--- src/services/mockAuth.js
+++ src/services/mockAuth.js
@@ -1,17 +1,28 @@
 import students from "./mockDatabase";
 
 function userAuth(username, password) {
-  let user = students.find((student) => {
-    return student.username === username && student.password === password;
-  });
+  // let user = students.find((student) => {
+  //   return student.username === username && student.password === password;
+  // });
 
-  if (!user) {
-    alert("Username or Password does not match");
-    return undefined;
-  }
+  // if (!user) {
+  //   alert("Username or Password does not match");
+  //   return undefined;
+  // }
+
+  // alert("You are logged in");
+  // return { ...user, isLoggedIn: true };
 
   alert("You are logged in");
-  return { ...user, isLoggedIn: true };
+
+  return {
+    name: "Sample Student",
+    username: "sample.student@example.com",
+    password: "sampstudent",
+    idNumber: "1234",
+    isLoggedIn: true,
+    requestedDocs: [],
+  };
 }
 
 export default userAuth;
