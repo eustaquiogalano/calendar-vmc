83200ebf7cbe2f443727ff7f6c803f5dcae3da8e
eustaquio <eustaquio.galano@gmail.com>
2025-12-18 17:44:59 +0800

    refactor: remove menu button on student component

--- src/components/PageMain/StudentUI/StudentUI.jsx
+++ src/components/PageMain/StudentUI/StudentUI.jsx
@@ -2,7 +2,6 @@ import style from "./StudentUI.module.css";
 import CalendarTab from "./CalendarTab/CalendarTab";
 import DocumentRequestTab from "./DocumentRequestTab/DocumentRequestTab";
 import { useState } from "react";
-import Icon from "./../../../assets/icons/menu.svg?react";
 
 function StudentUI({ student }) {
   const [tabSelected, setTabSelected] = useState(0);
@@ -20,11 +19,6 @@ function StudentUI({ student }) {
 
   return (
     <>
-      <button
-        className={`${style["studentUI__menu"]} ${style["studentUI__button"]}`}
-      >
-        <Icon className={style["studentUI__menu-icon"]} />
-      </button>
       <aside
         className={`${style["studentUI__aside"]} ${style["studentUI__aside--shadow"]}`}
       >
