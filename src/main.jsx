b0d8b3fe01b48d3189243052d1501ebdaff96694
eustaquio <eustaquio.galano@gmail.com>
2025-12-18 17:42:14 +0800

    feat: wrap App component with Menu context

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./App.css";
+import { MenuProvider } from "./context/MenuContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <MenuProvider>
+      <App />
+    </MenuProvider>
   </StrictMode>
 );
