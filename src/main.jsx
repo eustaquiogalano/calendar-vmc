18a2eaed8f75ee888e150aa052b01f8b167dd96c
eustaquio <eustaquio.galano@gmail.com>
2025-12-22 02:02:03 +0800

    feat: wrap App with RequestContext

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./App.css";
 import { MenuProvider } from "./context/MenuContext.jsx";
+import { RequestProvider } from "./context/RequestContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MenuProvider>
-      <App />
-    </MenuProvider>
+    <RequestProvider>
+      <MenuProvider>
+        <App />
+      </MenuProvider>
+    </RequestProvider>
   </StrictMode>
 );
