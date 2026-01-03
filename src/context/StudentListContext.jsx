18d3d2d6ca1423fed382ef5ca7ee21b24e4d3738
eustaquio <eustaquio.galano@gmail.com>
2026-01-04 01:57:36 +0800

    feat: add updateRequestStatus method to context

--- src/context/StudentListContext.jsx
+++ src/context/StudentListContext.jsx
@@ -6,8 +6,25 @@ const StudentListContext = createContext(null);
 export function StudentListProvider({ children }) {
   const [studentList, setStudentList] = useState([...mockStudentDatabase]);
 
+  function updateRequestStatus(studentID, requestID, status) {
+    const newList = studentList.map((student) => {
+      if (student.idNumber === studentID) {
+        const updatedRequests = student.requestedDocuments.map((request) => {
+          if (request.id === requestID) {
+            return { ...request, status: status };
+          }
+          return request;
+        });
+        return { ...student, requestedDocuments: updatedRequests };
+      }
+      return student;
+    });
+
+    setStudentList(newList);
+  }
+
   return (
-    <StudentListContext.Provider value={{ studentList }}>
+    <StudentListContext.Provider value={{ studentList, updateRequestStatus }}>
       {children}
     </StudentListContext.Provider>
   );
