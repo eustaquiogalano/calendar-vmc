b8a5b0c4ad5c9c099939fa7924b192d57dd2fbcc
eustaquio <eustaquio.galano@gmail.com>
2026-01-03 17:08:27 +0800

    feat: display student and requests

--- src/components/PageMain/AdminUI/IncomingRequestTab/IncomingRequestTab.jsx
+++ src/components/PageMain/AdminUI/IncomingRequestTab/IncomingRequestTab.jsx
@@ -1,11 +1,29 @@
+import { useStudentList } from "../../../../context/StudentListContext";
 import IncomingCard from "./IncomingCard/IncomingCard";
 import style from "./IncomingRequestTab.module.css";
 
 function IncomingRequestTab() {
+  const { studentList } = useStudentList();
+
   return (
     <section className={style["incoming-request__section"]}>
       <h2>Incoming Requests</h2>
-      <div className={style["incoming-request__request-list"]}></div>
+      <div className={style["incoming-request__request-list"]}>
+        {studentList.map((student) => {
+          return (
+            <div key={student.idNumber}>
+              <p>
+                Name: <span>{student.name}</span>
+              </p>
+              <div>
+                {student.requestedDocuments.map((request) => {
+                  return <IncomingCard key={request.id} request={request} />;
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 }
