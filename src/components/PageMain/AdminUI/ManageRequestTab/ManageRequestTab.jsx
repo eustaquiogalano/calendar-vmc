bed89dc094d74a25f0a1d2b836c73c2ae40373f6
eustaquio <eustaquio.galano@gmail.com>
2026-01-05 01:25:08 +0800

    feat: add ManageRequestTab initial layout

---
+++ src/components/PageMain/AdminUI/ManageRequestTab/ManageRequestTab.jsx
@@ -0,0 +1,44 @@
+import { useStudentList } from "../../../../context/StudentListContext";
+import AcceptedRequestCard from "./AcceptedRequestCard/AcceptedRequestCard";
+import style from "./ManageRequestTab.module.css";
+
+function ManageRequestTab() {
+  const { studentList } = useStudentList();
+
+  return (
+    <>
+      <section className={style["manage-request__section"]}>
+        <h2>Request View</h2>
+        <div className={style["manage-request__display-box"]}>
+          <p>Select a request to view here</p>
+        </div>
+      </section>
+      <section className={style["manage-request__section"]}>
+        <h2>Request List</h2>
+        <div className="manage-request__container--request-list">
+          {studentList.map((student) => {
+            return student.requestedDocuments
+              .filter((request) => {
+                if (request.status === "ACCEPTED") {
+                  return request;
+                }
+              })
+              .map((request) => {
+                console.log(request);
+
+                return (
+                  <AcceptedRequestCard
+                    key={request.id}
+                    request={request}
+                    student={student}
+                  />
+                );
+              });
+          })}
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default ManageRequestTab;
