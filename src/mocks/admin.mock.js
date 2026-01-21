a5005b078fcb11557015bcf5cd032f6a148f671b
eustaquio <eustaquio.galano@gmail.com>
2026-01-21 20:37:53 +0800

    chore: add student and admin mock database

---
+++ src/mocks/admin.mock.js
@@ -0,0 +1,22 @@
+const mockAdminDatabase = [
+  {
+    userType: "admin",
+    username: "dean.office",
+    password: "deanmalakas",
+    email: "dean.office@gmail.com",
+    name: "Dean",
+    idNumber: "1234",
+    isLoggedIn: false,
+  },
+  {
+    userType: "admin",
+    username: "registrar.office",
+    password: "registrarmabilis",
+    email: "registrar.office@gmail.com",
+    name: "Registrar",
+    idNumber: "12345",
+    isLoggedIn: false,
+  },
+];
+
+export default mockAdminDatabase;
