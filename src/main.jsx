85e1fa15e0a3d31c99b8a8d4f466d5b29f2b6ec7
eustaquio <eustaquio.galano@gmail.com>
2026-02-02 12:43:26 +0800

    feat: handle initial routing setup
    - Added root route
    - Added error page

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./App.css";
 import { MenuProvider } from "./context/MenuContext.jsx";
 import { RequestProvider } from "./context/RequestContext.jsx";
 import { StudentListProvider } from "./context/StudentListContext.jsx";
 import { EventsProvider } from "./context/EventsContext.jsx";
+import ErrorPage from "./components/ErrorPage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -13,7 +23,7 @@ createRoot(document.getElementById("root")).render(
       <StudentListProvider>
         <RequestProvider>
           <MenuProvider>
-            <App />
+            <RouterProvider router={router} />
           </MenuProvider>
         </RequestProvider>
       </StudentListProvider>
