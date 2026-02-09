001621a5f02b4a2c6531d120256116e1a79b2843
eustaquio <eustaquio.galano@gmail.com>
2026-02-09 13:49:18 +0800

    chore: move unused files to trash folder

---
+++ src/trash/PageHeader/PageHeader.jsx
@@ -0,0 +1,26 @@
+import vmcIcon from "../../assets/images/vmc-icon.png";
+import style from "./PageHeader.module.css";
+import Icon from "./../../assets/icons/menu.svg?react";
+import { useMenu } from "../../context/MenuContext";
+
+function PageHeader() {
+  const { menuButtonVisibility, toggleMenuBody } = useMenu();
+
+  return (
+    // <header className={style["page-header"]}>
+    //   <img className={style["page-header__img"]} src={vmcIcon} alt="" />
+    //   <h1 className={style["page-header__h1"]}>Services</h1>
+    //   <button
+    //     onClick={toggleMenuBody}
+    //     className={`${style["studentUI__menu"]} ${style["studentUI__button"]} ${
+    //       menuButtonVisibility ? "" : style["studentUI__menu--hide"]
+    //     }`}
+    //   >
+    //     <Icon className={style["studentUI__menu-icon"]} />
+    //   </button>
+    // </header>
+    <p>No longer used</p>
+  );
+}
+
+export { PageHeader };
---
+++ src/trash/PageMain.jsx
@@ -0,0 +1,46 @@
+import { StudentUI } from "../components/PageMain/StudentUI/StudentUI";
+import style from "./PageMain.module.css";
+import Login from "../components/PageMain/Login/Login";
+import { useState } from "react";
+import { useMenu } from "../context/MenuContext";
+import AdminUI from "../components/PageMain/AdminUI/AdminUI";
+import { Outlet } from "react-router-dom";
+
+function PageMain() {
+  const [student, setStudent] = useState(undefined);
+  const [admin, setAdmin] = useState(undefined);
+  const { toggleMenuButton } = useMenu();
+
+  // function handleLogin(user) {
+  //   if (user.userType === "admin") {
+  //     setAdmin(user);
+  //   } else if (user.userType === "student") {
+  //     setStudent(user);
+  //   }
+
+  //   if (user) {
+  //     toggleMenuButton();
+  //   }
+  // }
+
+  // function handleMainDisplay() {
+  //   if (student) {
+  //     return <StudentUI student={student} setStudent={setStudent} />;
+  //   } else if (admin) {
+  //     return <AdminUI admin={admin} setAdmin={setAdmin} />;
+  //   } else {
+  //     return <Login onLogin={handleLogin} />;
+  //   }
+  // }
+
+  // return (
+  //   <main
+  //     className={`${style["page-main"]} ${style["page-main--hide-scroll"]}`}
+  //   >
+  //     {/* {handleMainDisplay()} */}
+  //     <Outlet />
+  //   </main>
+  // );
+}
+
+export { PageMain };
