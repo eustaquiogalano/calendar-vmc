665db144ecac2fdcd82cac42c6a1427cbff04529
eustaquio <eustaquio.galano@gmail.com>
2026-03-08 18:31:33 +0800

    feat: initialize and persist events

--- src/components/PageMain/StudentUI/CalendarTab/Calender/Calendar.jsx
+++ src/components/PageMain/StudentUI/CalendarTab/Calender/Calendar.jsx
@@ -2,18 +2,42 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import "./Calendar.css";
+import { getEvents } from "../../../../../utils/storage";
+import { useEffect, useState } from "react";
 
 function Calendar({ requests, setSelectedDate }) {
-  const events = requests.map((request) => {
-    return {
-      title: request.document,
-      start: request.date,
-      allDay: true,
-      extendedProps: {
-        request,
-      },
-    };
-  });
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    async function initEvents() {
+      const studentEvents = requests.map((request) => {
+        return {
+          title: request.document,
+          start: request.date,
+          allDay: true,
+          extendedProps: {
+            request,
+          },
+        };
+      });
+
+      let schoolEvents = await getEvents();
+      let updatedSchoolEvents = schoolEvents.map((event) => {
+        return {
+          title: event.name,
+          start: event.date,
+          allDay: true,
+          extendedProps: {
+            event,
+          },
+        };
+      });
+
+      setEvents([...events, ...updatedSchoolEvents, ...studentEvents]);
+    }
+
+    initEvents();
+  }, []);
 
   function handleDateClick(info) {
     setSelectedDate(info.dateStr);
