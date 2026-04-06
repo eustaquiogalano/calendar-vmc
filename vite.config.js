2efd4e5b7739091d110d3f7787020d8d60eb2734
eustaquio <eustaquio.galano@gmail.com>
2026-04-06 11:25:18 +0800

    chore: install React Compiler and configure Vite
    - Add React Compiler to dependencies
    - Update vite.config to enable compiler plugin

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+    svgr(),
+  ],
   base: "/", // replace with repo name
   test: {
     environment: "jsdom",
