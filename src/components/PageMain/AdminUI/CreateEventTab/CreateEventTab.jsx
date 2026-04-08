3b1ccd2659dc8421c6faba2ac1610e80ed557439
eustaquio <eustaquio.galano@gmail.com>
2026-04-08 12:22:16 +0800

    refactor: use shadcn components in CreateEventTab
    - convert native components to shadcn components

--- src/components/PageMain/AdminUI/CreateEventTab/CreateEventTab.jsx
+++ src/components/PageMain/AdminUI/CreateEventTab/CreateEventTab.jsx
@@ -3,11 +3,42 @@ import { useEvent } from "../../../../context/EventsContext";
 import style from "./CreateEventTab.module.css";
 import { addEvent, deleteEvent } from "../../../../utils/storage";
 
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldLegend,
+  FieldSeparator,
+  FieldSet,
+} from "@/components/ui/field";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { format } from "date-fns";
+import { ChevronDownIcon } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 function CreateEvent() {
   const { events, setEvents } = useEvent();
   const [name, setName] = useState("");
   const [date, setDate] = useState("");
-  const [time, setTime] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+
+  console.log(date);
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -15,8 +46,9 @@ function CreateEvent() {
     const updatedEvents = await addEvent({
       id: crypto.randomUUID(),
       name,
-      time,
-      date,
+      startTime,
+      endTime,
+      date: format(date, "PPP"),
     });
     setEvents(updatedEvents);
     resetState();
@@ -29,7 +61,8 @@ function CreateEvent() {
   }
 
   function resetState() {
-    setTime("");
+    setStartTime("");
+    setEndTime("");
     setName("");
     setDate("");
   }
@@ -38,43 +71,84 @@ function CreateEvent() {
     <>
       <section className={style["create-event__section"]}>
         <h2>Create Event</h2>
-        <form onSubmit={handleSubmit} className={style["create-event__form"]}>
-          <div className={style["create-event__form-field"]}>
-            <label htmlFor="event-name">Event Name:</label>
-            <input
-              value={name}
-              onChange={(e) => {
-                setName(e.target.value);
-              }}
-              type="text"
-              id="event-name"
-            />
-          </div>
-          <div className={style["create-event__form-field"]}>
-            <label htmlFor="event-time">Time:</label>
-            <input
-              onChange={(e) => {
-                setTime(e.target.value);
-              }}
-              value={time}
-              type="time"
-              id="event-time"
-            />
-          </div>
-          <div className={style["create-event__form-field"]}>
-            <label htmlFor="event-date">Date:</label>
-            <input
-              onChange={(e) => {
-                setDate(e.target.value);
-              }}
-              value={date}
-              type="date"
-              id="event-date"
-            />
-          </div>
-          <div>
-            <button type="submit">Create</button>
-          </div>
+        <form onSubmit={handleSubmit}>
+          <FieldGroup>
+            <FieldSet>
+              <FieldGroup>
+                <Field>
+                  <FieldLabel htmlFor="checkout-7j9-card-name-43j">
+                    Event Name:
+                  </FieldLabel>
+                  <Input
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    id="checkout-7j9-card-name-43j"
+                    placeholder="Basketball"
+                    required
+                  />
+                </Field>
+
+                <FieldGroup className="flex flex-row">
+                  <Field>
+                    <FieldLabel htmlFor="checkout-7j9-card-number-uw1">
+                      Start Time:
+                    </FieldLabel>
+                    <Input
+                      value={startTime}
+                      onChange={(e) => setStartTime(e.target.value)}
+                      type="time"
+                      id="checkout-7j9-card-number-uw1"
+                      placeholder="1234 5678 9012 3456"
+                      required
+                    />
+                  </Field>
+                  <Field>
+                    <FieldLabel htmlFor="checkout-7j9-card-number-uw1">
+                      End Time:
+                    </FieldLabel>
+                    <Input
+                      value={endTime}
+                      onChange={(e) => setEndTime(e.target.value)}
+                      type="time"
+                      id="checkout-7j9-card-number-uw1"
+                      placeholder="1234 5678 9012 3456"
+                      required
+                    />
+                  </Field>
+                </FieldGroup>
+
+                <Field>
+                  <FieldLabel htmlFor="checkout-7j9-card-number-uw1">
+                    Date:
+                  </FieldLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant="outline"
+                        data-empty={!date}
+                        className="w-[212px] justify-between text-left font-normal data-[empty=true]:text-muted-foreground"
+                      >
+                        {date ? format(date, "PPP") : <span>Pick a date</span>}
+                        <ChevronDownIcon />
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={date}
+                        onSelect={setDate}
+                        defaultMonth={date}
+                      />
+                    </PopoverContent>
+                  </Popover>
+                </Field>
+              </FieldGroup>
+            </FieldSet>
+            <FieldSeparator></FieldSeparator>
+            <Field>
+              <Button type="submit">Create Event</Button>
+            </Field>
+          </FieldGroup>
         </form>
       </section>
       <section
@@ -85,22 +159,54 @@ function CreateEvent() {
           {events &&
             events.map((event) => {
               return (
-                <div className={style["create-event__event-card"]}>
-                  <p>
-                    Event Name: <span>{event.name}</span>
-                  </p>
-                  <p>
-                    Time: <span>{event.time}</span>
-                  </p>
-                  <p>
-                    Date: <span>{event.date}</span>
-                  </p>
-                  <div>
-                    <button onClick={() => handleDelete(event.id)}>
+                // <div className={style["create-event__event-card"]}>
+                //   <p>
+                //     Event Name: <span>{event.name}</span>
+                //   </p>
+                //   <p>
+                //     Time: <span>{event.endTime}</span>
+                //   </p>
+                //   <p>
+                //     Time: <span>{event.startTime}</span>
+                //   </p>
+                //   <p>
+                //     Date: <span>{event.date}</span>
+                //   </p>
+                //   <div>
+                //     <button onClick={() => handleDelete(event.id)}>
+                //       Delete
+                //     </button>
+                //   </div>
+                // </div>
+                <Card className="border-2 h-full">
+                  <CardHeader>
+                    <CardTitle>{event.name}</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="flex justify-between">
+                      <span className="text-muted-foreground">Start Time:</span>
+                      <span>{event.startTime}</span>
+                    </div>
+                    <div className="flex justify-between">
+                      <span className="text-muted-foreground">End Time:</span>
+                      <span>{event.endTime}</span>
+                    </div>
+                    <div className="flex justify-between">
+                      <span className="text-muted-foreground">Date:</span>
+                      <span>{event.date}</span>
+                    </div>
+                  </CardContent>
+                  <CardFooter>
+                    <Button
+                      variant="default"
+                      size="sm"
+                      className="w-full"
+                      onClick={() => handleDelete(event.id)}
+                    >
                       Delete
-                    </button>
-                  </div>
-                </div>
+                    </Button>
+                  </CardFooter>
+                </Card>
               );
             })}
         </div>
