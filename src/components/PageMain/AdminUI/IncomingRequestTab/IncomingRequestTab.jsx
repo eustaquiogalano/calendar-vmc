93db822393d4ab0cfdf22ab8487318c547efa655
eustaquio <eustaquio.galano@gmail.com>
2026-04-10 22:51:27 +0800

    refactor: apply color palette to buttons

--- src/components/PageMain/AdminUI/IncomingRequestTab/IncomingRequestTab.jsx
+++ src/components/PageMain/AdminUI/IncomingRequestTab/IncomingRequestTab.jsx
@@ -66,7 +66,7 @@ function IncomingRequestTab() {
                       Accept
                     </Button>
                     <Button
-                      variant="default"
+                      variant="secondary"
                       size="sm"
                       onClick={() =>
                         updateRequestStatus(
