c650367240d9d4ebf9f1c945cb27a267c84fadda
eustaquiogalano <135113924+eustaquiogalano@users.noreply.github.com>
2026-05-03 15:24:37 +0800

    refactor(auth): remove unused card description

--- src/components/PageMain/AuthForm/AuthForm.jsx
+++ src/components/PageMain/AuthForm/AuthForm.jsx
@@ -276,11 +276,6 @@ export function LoginForm({ className, ...props }) {
           </form>
         </CardContent>
       </Card>
-
-      <FieldDescription className="px-6 text-center">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
-      </FieldDescription>
     </div>
   );
 }
