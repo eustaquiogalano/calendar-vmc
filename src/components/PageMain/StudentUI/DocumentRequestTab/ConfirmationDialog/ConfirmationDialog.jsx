5e4083a605109724770035dec08b5d77095dea8e
eustaquiogalano <135113924+eustaquiogalano@users.noreply.github.com>
2026-05-16 10:22:42 +0800

    refactor: update ConfirmationDialog styling

--- src/components/PageMain/StudentUI/DocumentRequestTab/ConfirmationDialog/ConfirmationDialog.jsx
+++ src/components/PageMain/StudentUI/DocumentRequestTab/ConfirmationDialog/ConfirmationDialog.jsx
@@ -1,31 +1,51 @@
 import { forwardRef } from "react";
 import style from "./ConfirmationDialog.module.css";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { IoWarningOutline } from "react-icons/io5";
 
 const ConfirmationDialog = forwardRef(function ConfirmationDialog(
   { onConfirm },
-  ref
+  ref,
 ) {
   return (
     <dialog className={style["confirmation-dialog"]} ref={ref}>
-      <p>Do you want to delete the request?</p>
+      <Card className="border-2 border-destructive">
+        <CardHeader className="flex flex-col items-center px-5">
+          <IoWarningOutline size={30} className="text-destructive" />
+          <CardTitle className="text-center">Confirm Deletion</CardTitle>
+        </CardHeader>
 
-      <div className={style["confirmation-dialog__button-container"]}>
-        <button
-          className={style["confirmation-dialog__button"]}
-          onClick={() => {
-            onConfirm();
-            ref.current.close();
-          }}
-        >
-          Delete
-        </button>
-        <button
-          className={style["confirmation-dialog__button"]}
-          onClick={() => ref.current.close()}
-        >
-          Cancel
-        </button>
-      </div>
+        <CardContent className="px-5">
+          <p>Do you want to delete the request?</p>
+        </CardContent>
+
+        <CardFooter className="flex justify-between gap-2 px-5">
+          <Button
+            variant="destructive"
+            className=" w-[50%] h-fit p-2"
+            onClick={() => {
+              onConfirm();
+              ref.current.close();
+            }}
+          >
+            Delete
+          </Button>
+          <Button
+            variant="outline"
+            className="w-[50%] h-fit p-2"
+            onClick={() => ref.current.close()}
+          >
+            Cancel
+          </Button>
+        </CardFooter>
+      </Card>
     </dialog>
   );
 });
