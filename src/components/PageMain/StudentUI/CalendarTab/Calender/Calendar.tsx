dbc69603d267a4a42166f0e35b7ee7e632c54b97
eustaquiogalano <135113924+eustaquiogalano@users.noreply.github.com>
2026-07-10 16:18:34 +0800

    chore: remove unused codes

--- src/components/PageMain/StudentUI/CalendarTab/Calender/Calendar.tsx
+++ src/components/PageMain/StudentUI/CalendarTab/Calender/Calendar.tsx
@@ -2,9 +2,6 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
 import "./Calendar.css";
-import { getEvents } from "../../../../../utils/storage/storage";
-import { useEffect, useState } from "react";
-import { format, parse } from "date-fns";
 import { DocumentRequest } from "../../../../../types/documentRequest";
 import { SchoolEvent } from "../../../../../types/schoolEvent";
 import { useDocumentRequest } from "@/context/DocumentRequestContext";
@@ -26,40 +23,8 @@ function Calendar({
 }: {
   setSelectedDate: (date: string) => void;
 }) {
-  // const [events, setEvents] = useState([]);
   const { requests } = useDocumentRequest();
   const { events: schoolEvents } = useEvent();
-
-  // useEffect(() => {
-  //   async function initEvents() {
-  //     const studentEvents = requests.map((request) => {
-  //       return {
-  //         title: request.document,
-  //         start: request.date,
-  //         allDay: true,
-  //         extendedProps: {
-  //           request,
-  //         },
-  //       };
-  //     });
-
-  //     // let schoolEvents = await getEvents();
-  //     let updatedSchoolEvents = schoolEvents.map((event) => {
-  //       return {
-  //         title: event.name,
-  //         start: event.date,
-  //         allDay: true,
-  //         extendedProps: {
-  //           event,
-  //         },
-  //       };
-  //     });
-
-  //     setEvents([...events, ...updatedSchoolEvents, ...studentEvents]);
-  //   }
-
-  //   initEvents();
-  // }, []);
 
   const calendarEvents: CalendarEvent[] = [
     ...requests.map((request) => ({
