42c9bb9099b35bb2c192f9b7689a2cad26d6eed3
eustaquiogalano <135113924+eustaquiogalano@users.noreply.github.com>
2026-07-13 15:29:02 +0800

    refactor: migrate from JS to TS

--- src/components/RejectRequestDialog/RejectRequestDialog.tsx
+++ src/components/RejectRequestDialog/RejectRequestDialog.tsx
@@ -10,10 +10,16 @@ import {
 import { Button } from "@/components/ui/button";
 import { IoWarningOutline } from "react-icons/io5";
 
-const RejectRequestDialog = forwardRef(function RejectRequestDialog(
-  { onRejection },
-  ref,
-) {
+interface RejectRequestDialogProps {
+  onRejection: () => void; // adjust the type if onRejection takes parameters
+}
+
+const RejectRequestDialog = forwardRef<
+  HTMLDialogElement,
+  RejectRequestDialogProps
+>(function RejectRequestDialog({ onRejection }, ref) {
+  const dialogRef = ref as React.RefObject<HTMLDialogElement>;
+
   return (
     <dialog className={style["confirmation-dialog"]} ref={ref}>
       <Card className="border-2 border-destructive">
@@ -32,7 +38,7 @@ const RejectRequestDialog = forwardRef(function RejectRequestDialog(
             className=" w-[50%] h-fit p-2"
             onClick={() => {
               onRejection();
-              ref.current.close();
+              dialogRef.current.close();
             }}
           >
             Reject
@@ -40,7 +46,7 @@ const RejectRequestDialog = forwardRef(function RejectRequestDialog(
           <Button
             variant="outline"
             className="w-[50%] h-fit p-2"
-            onClick={() => ref.current.close()}
+            onClick={() => dialogRef.current.close()}
           >
             Cancel
           </Button>
