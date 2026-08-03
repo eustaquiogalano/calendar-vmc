1b67120eba1d65dae8d04b84002776276809aeaa
eustaquiogalano <135113924+eustaquiogalano@users.noreply.github.com>
2026-08-03 09:48:03 +0800

    chore: add comments for explanation

--- src/components/StatusStepper/StatusStepper.tsx
+++ src/components/StatusStepper/StatusStepper.tsx
@@ -182,15 +182,20 @@ export function StatusStepper({ request }: StatusStepperProps) {
   return (
     <div className="flex flex-col bg-secondary p-4 rounded-2xl border">
       {steps.map((step, index) => {
+        // if this is true check
+        // the cirle of the current step
         const isCompleted = isRejected
           ? ["PENDING", "ACCEPTED"].includes(step.key)
-          : statusOrder.indexOf(step.key) < currentIndex;
+          : statusOrder.indexOf(step.key) <= currentIndex;
+
         const isCurrent = step.key === request.status;
         const isLast = index === steps.length - 1;
 
         return (
-          <div key={step.key} className="flex gap-3 items-start">
+          <div key={step.key} className="flex gap-3 items-start ">
+            {/* icon and line connector */}
             <div className="flex flex-col items-center">
+              {/* Circle with check */}
               <div
                 className={`w-5 h-5 rounded-full shrink-0 flex items-center justify-center
                   ${
@@ -207,6 +212,7 @@ export function StatusStepper({ request }: StatusStepperProps) {
                 )}
               </div>
 
+              {/* line connector */}
               {!isLast && (
                 <div
                   className={`w-0.5 h-10 ${isCompleted ? "bg-blue-600" : "bg-gray-200"}`}
@@ -214,6 +220,7 @@ export function StatusStepper({ request }: StatusStepperProps) {
               )}
             </div>
 
+            {/* status and description */}
             <div className={`pt-0.5 ${isLast ? "pb-0" : "pb-3"}`}>
               <p
                 className={`text-sm font-medium mb-0.5
